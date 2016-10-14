25ee9cb39873d825a357f348cd449f56e4e30365
nerdy314 <mattweb117@gmail.com>
2016-10-14 12:00:44 -0700

    Need to test out the threading

--- Assignment 2/threaded-echo-server.py
+++ Assignment 2/threaded-echo-server.py
@@ -11,43 +11,44 @@ HTTP_BAD_METHOD = "HTTP/1.1 405 HTTP_BAD_METHOD"
 CACHE = []
 
 
-def receive_request(conn):
+def receive_request(connection):
     request_buffer = ''
     while "\r\n\r\n" not in request_buffer or "\n\n" not in request_buffer:
-        request_buffer += conn.recv(BUFFER_SIZE)
+        request_buffer += connection.recv(BUFFER_SIZE)
     return request_buffer.split("\n") if "\n\n" in request_buffer else request_buffer.split("\r\n")
 
 
 def parse_url(url):
     http_pos = url.find("://")
-    webserver = url[(http_pos + 3):] if http_pos != -1 else webserver = url
+    webserver = url[(http_pos + 3):] if http_pos != -1 else url
     return webserver.split("/", 1)
 
 
-def handler(conn):
-    lines = receive_request(conn)
+def handler(connection):
+    lines = receive_request(connection)
     header, url, httpv = lines[0].split(" ")
     if GET_HEADER not in header:
-        conn.sendall(HTTP_BAD_METHOD)
+        connection.sendall(HTTP_BAD_METHOD)
     webserver, target_file = parse_url(url)
     proxy = create_connection(webserver, DEFAULT_PORT)
     lines = map(lambda x: CONNECTION_CLOSE if "Connection" in x else x, lines)
     request = GET_HEADER + " " + target_file + " " + httpv + ''.join(lines[1:])
     proxy.sendall(request)
-    #     Receive new data
 
+    #     Receive new data from webserver
     response = "\n".join(receive_request(proxy))
-    conn.sendall(response)
+    connection.sendall(response)
+    proxy.close()
 
 
 def create_connection(webserver, port):
     t = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    t.connect((webserver, port))
+    t.bind((webserver, port))
     return t
 
 
-def main():
-    server_port = sys.argv[1]
+if __name__ == '__main__':
+    server_port = int(sys.argv[1])
     host_name = socket.gethostname()
     host_ip = socket.gethostbyname(host_name)
     s = create_connection(host_ip, server_port)
