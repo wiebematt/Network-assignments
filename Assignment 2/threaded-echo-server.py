f79bea7d713969326467680b046c9f24da56a470
Matthew Wiebe <mattweb117@gmail.com>
2016-10-14 18:18:53 -0700

    Got caching and browsing to work

--- Assignment 2/threaded-echo-server.py
+++ Assignment 2/threaded-echo-server.py
@@ -6,14 +6,14 @@ import sys
 BUFFER_SIZE = 1024
 DEFAULT_PORT = 80
 GET_HEADER = "GET"
-CONNECTION_CLOSE = "Connection: close\r\n"
+CONNECTION_CLOSE = "Connection: close"
 HTTP_BAD_METHOD = "HTTP/1.1 405 HTTP_BAD_METHOD"
-CACHE = []
+CACHE = {}
 
 
 def receive_request(connection):
     request_buffer = ''
-    while "\r\n\r\n" not in request_buffer or "\n\n" not in request_buffer:
+    while "\r\n\r\n" not in request_buffer and "\n\n" not in request_buffer:
         request_buffer += connection.recv(BUFFER_SIZE)
     return request_buffer.split("\n") if "\n\n" in request_buffer else request_buffer.split("\r\n")
 
@@ -30,15 +30,21 @@ def handler(connection):
     if GET_HEADER not in header:
         connection.sendall(HTTP_BAD_METHOD)
     webserver, target_file = parse_url(url)
-    proxy = create_connection(webserver, DEFAULT_PORT)
-    lines = map(lambda x: CONNECTION_CLOSE if "Connection" in x else x, lines)
-    request = GET_HEADER + " " + target_file + " " + httpv + ''.join(lines[1:])
-    proxy.sendall(request)
+    if (webserver, target_file) in CACHE.keys():
+        connection.sendall(CACHE[(webserver, target_file)])
+        print "Cache Hit"
+    else:
+        proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        proxy.connect((socket.gethostbyname(webserver), DEFAULT_PORT))
+        lines = map(lambda x: CONNECTION_CLOSE if "Connection" in x else x, lines)
+        request = GET_HEADER + " /" + target_file + " " + httpv + "\n" + '\n'.join(lines[1:])
+        proxy.sendall(request)
 
-    #     Receive new data from webserver
-    response = "\n".join(receive_request(proxy))
-    connection.sendall(response)
-    proxy.close()
+        # Receive new data from webserver
+        response = "\n".join(receive_request(proxy))
+        CACHE[(webserver, target_file)] = response
+        connection.sendall(response)
+        proxy.close()
 
 
 def create_connection(webserver, port):
@@ -53,8 +59,9 @@ if __name__ == '__main__':
     host_ip = socket.gethostbyname(host_name)
     s = create_connection(host_ip, server_port)
     s.listen(5)
+    print "Listening..."
 
     while True:
         conn, addr = s.accept()
-        print 'Server connected by', addr,
+        print 'Server connected by', addr, "\n"
         thread.start_new(handler, (conn,))
